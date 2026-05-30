e5b51ee470f041520bee78f67540fdcfe4cb4c31
Mark Kulishov <mark.tabletpc@gmail.com>
2026-05-30 21:01:27 +0300

    feat: Task 7 (Authorization)

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -28,12 +28,17 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
 
     if (!file) return;
 
+    const authorizationToken = localStorage.getItem("authorization_token");
+
     const response = await axios({
       method: "GET",
       url,
       params: {
         name: encodeURIComponent(file.name),
       },
+      headers: authorizationToken
+        ? { Authorization: `Basic ${authorizationToken}` }
+        : undefined,
     });
     console.log("File to upload: ", file.name);
     console.log("Uploading to: ", response.data);
