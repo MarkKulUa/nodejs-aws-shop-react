2b03b9328b30b71a0f72fdc12a413f2830660a5e
Mark Kulishov <mark.tabletpc@gmail.com>
2026-06-06 22:45:35 +0300

    feat: Task 8 (Integration with SQL Database)

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
     order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://7wmdmkf96g.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://ibnol00ub5.execute-api.eu-west-1.amazonaws.com/prod/api",
 };
 
 export default API_PATHS;
